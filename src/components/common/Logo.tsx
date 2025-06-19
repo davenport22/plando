c4d4b70af229ff359413d9ec4e4187485c8317ea
David Schneiderbauer <david.m.schneiderbauer@gmail.com>
2025-06-19 14:49:48 +0000

    everything we have implemented now is called plando travel --> change th

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <Link href="/" className="flex items-center gap-2 text-primary hover:opacity-80 transition-opacity">
       <PlaneTakeoff className="h-8 w-8" />
-      <span className="text-2xl font-headline font-semibold">Plando</span>
+      <span className="text-2xl font-headline font-semibold">Plando Travel</span>
     </Link>
   );
 };
