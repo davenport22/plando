775964c5234d9faad9a62424e4d650dff1f4920b
David Schneiderbauer <david.m.schneiderbauer@gmail.com>
2025-07-05 18:25:28 +0000

    the generate itinery function does not work yet

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,9 +1,8 @@
 
-// import { config } from 'dotenv';
-// config();
+import { config } from 'dotenv';
+config();
 
 import '@/ai/flows/generate-suggested-itinerary.ts';
 import '@/ai/flows/generate-activity-description-flow.ts';
 import '@/ai/flows/generate-destination-image-flow.ts';
 import '@/ai/flows/generate-invitation-email-flow.ts';
-
