c382bf285fc9cf354848c661c12029d7d1b7642f
David Schneiderbauer <david.m.schneiderbauer@gmail.com>
2025-07-13 18:17:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/common/CitySelect.tsx
+++ src/components/common/CitySelect.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import * as React from "react";
+import { useFormContext } from "react-hook-form";
 import { CITIES } from "@/lib/cities";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { FormControl } from "@/components/ui/form";
@@ -12,13 +13,35 @@ interface CitySelectProps {
 }
 
 export function CitySelect({ onValueChange, defaultValue }: CitySelectProps) {
+  // Check if the component is used inside a FormProvider
+  const form = useFormContext();
+
+  // If it's part of a form, wrap it in a FormControl to get form styling and context
+  if (form) {
+    return (
+      <Select onValueChange={onValueChange} defaultValue={defaultValue}>
+        <FormControl>
+          <SelectTrigger>
+            <SelectValue placeholder="Select a city" />
+          </SelectTrigger>
+        </FormControl>
+        <SelectContent>
+          {CITIES.map((city) => (
+            <SelectItem key={city} value={city}>
+              {city}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    );
+  }
+
+  // Otherwise, render it as a standalone Select component
   return (
     <Select onValueChange={onValueChange} defaultValue={defaultValue}>
-      <FormControl>
-        <SelectTrigger>
-          <SelectValue placeholder="Select a city" />
-        </SelectTrigger>
-      </FormControl>
+      <SelectTrigger>
+        <SelectValue placeholder="Select a city" />
+      </SelectTrigger>
       <SelectContent>
         {CITIES.map((city) => (
           <SelectItem key={city} value={city}>
